ed4dafdfcb0121b21b619fe7b52a8174131316e8
github username <github email address>
2020-01-22 09:43:20 -0800

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -2,8 +2,57 @@ require_relative "../config/environment.rb"
 
 class Student
 
+  attr_accessor :name, :grade
+  attr_reader :id
+
+  def initialize(name, grade, id=nil)
+    @name = name
+    @grade = grade 
+    @id = id
+  end
+
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+      )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE IF EXISTS students
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def save
+
+  end
+
+  def self.create
+
+  end 
+
+  def self.new_from_db
+
+  end
+
+  def self.find_by_name
+
+  end
+
+  def update
+
+  end
+
+
 
 
 end
